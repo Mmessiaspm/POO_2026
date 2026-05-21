f63d8a84a630bb3c585aae05e645edd04dd8ec96
Mmessiaspm <mmessias@ifpi.edu.br>
2026-05-21 08:43:09 -0300

    Aula Interface 21/05/2026

---
+++ Cliente.java
@@ -0,0 +1,22 @@
+public class Cliente extends Pessoa {
+
+     private String endereco;
+     private String telefone;
+
+     public String getEndereco() {
+         return this.endereco;
+     }
+
+     public String getTelefone() {
+         return this.telefone;
+     }
+
+     public void setEndereco(String endereco) {
+         this.endereco = endereco;
+     }
+
+     public void setTelefone(String telefone) {
+         this.telefone = telefone;
+     }
+
+}
