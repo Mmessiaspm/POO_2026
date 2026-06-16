963df4e712ed2bbf79c11b00fb146dc9018f2f61
Mmessiaspm <mmessias@ifpi.edu.br>
2026-06-16 08:58:05 -0300

    AULA 16/06/2026 - MÉTODO CONSTRUTO E ATRUBUT DE CLASSE

--- AumentaFrase.java
+++ AumentaFrase.java
@@ -3,15 +3,19 @@ public class AumentaFrase {
 
     public static void main(String[] args) {
         String novaFrase = null;
-        String frase = null;
+        String frase =null;
         try{
             novaFrase = frase.toUpperCase();
         } catch (NullPointerException e) {
             System.out.println("A frase é nula. Por favor, inicialize a frase antes de usar.");
+            frase = "Frase padrão";
         }
-        
-        System.out.println(novaFrase);
-        System.out.println(frase);
+        finally {
+            novaFrase = frase.toUpperCase();
+            System.out.println(novaFrase);
+            System.out.println(frase);
+        }
     }
+
 
 }
